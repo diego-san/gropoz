2af7849b5c60b0acc820436c1e97bb9802c5fd9e
levo <diego.sanchez@alumnos.upla.cl>
2018-06-17 17:02:20 -0400

    inicio pagina web test

--- index.php
+++ index.php
@@ -49,31 +49,58 @@
     <div class="collapse navbar-collapse justify-content-md-center" id="nav-content">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" data-scroll href="#sec-1" >Portfolio</a>
+                <a class="nav-link nav-menu" data-scroll href="#sec-1" >Portfolio</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" data-scroll href="#sec-2" >About</a>
+                <a class="nav-link nav-menu" data-scroll href="#sec-2" >About</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" data-scroll href="#sec-3" >Link 3</a>
+                <a class="nav-link nav-menu" data-scroll href="#sec-3" >Contact</a>
             </li>
         </ul>
 </nav>
 
 <!--eccion 1-->
-
-
+<header>
     <div class="parallax-window" data-parallax="scroll" data-image-src="img/fondo.png" data-z-index="1" id="sec-1">
-        <p class="parallax-window-into text-center tlt ">Gropoz Web Developer</p>
-        <p class="parallax-window-into text-center tlt ">Start Bootstrap</p>
-        <button type="button" class="btn btn-primary boton-parallax tlt ">Repository</button>
+<div class="container " >
+    <div class="row">
+        <div class="col-md-12 space-parallax-into"></div>
+        <div class="col-md-12 parallax-window-into">
+            <p class="h1 parallax-window-into-text text-center tlt">Gropoz</p>
+            <p class="h1 parallax-window-into-text text-center tlt">Web Developer in bootstrap</p>
+            <button type="button" class="btn btn-primary boton-parallax tlt">Repository</button>
+        </div>
     </div>
-
-<!--eccion 2-->
-<div class="container-fluid seccion" style="background: #0062cc" id="sec-2"></div>
 </div>
+    </div>
+</header>
+<!--eccion 2-->
+<div class="container-fluid seccion"  id="sec-2">
+    <div class="row">
+        <div class="col-md-6  sec-2-into-block-2">
+            <img src="img/responsive-prime.png" class="img-fluid" alt="Responsive image">
+        </div>
+        <div class="col-md-6 sec-2-into-block-2" style="background: #212529;">
+            <p class="h1 sec-2-into-bock-text text-center">ABOUT</p>
+            <p class="h3  sec-2-into-bock-text2 " >Newly improved, and full of great utility classes, Bootstrap 4 is leading the way in mobile responsive web development! All of the themes on Start Bootstrap are now using Bootstrap 4!</p>
+        </div>
+    </div>
+</div>
+
 <!--eccion 3-->
-<div class="container-fluid seccion" style="background: #cc88ff" id="sec-3"></div>
+<div class="container-fluid seccion"  id="sec-3">
+    <div class="row">
+        <div class="col-md-6 sec-2-into-block-2" style="background: #212529;">
+            <p class="h1 sec-2-into-bock-text text-center">ABOUT</p>
+            <p class="h3  sec-2-into-bock-text2 " >Newly improved, and full of great utility classes, Bootstrap 4 is leading the way in mobile responsive web development! All of the themes on Start Bootstrap are now using Bootstrap 4!</p>
+        </div>
+        <div class="col-md-6  sec-2-into-block-2">
+            <img src="img/contacto.jpg" class="img-fluid" alt="Responsive image">
+        </div>
+
+    </div>
+</div>
 
 <footer>
     <div class="contaider-fluid footer"></div>
